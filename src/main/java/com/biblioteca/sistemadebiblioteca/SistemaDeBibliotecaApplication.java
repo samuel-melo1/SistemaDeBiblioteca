3165623b19fca40592049d9ff172a09679c92205
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-11-16 22:41:05 -0300

    initial project

---
+++ src/main/java/com/biblioteca/sistemadebiblioteca/SistemaDeBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.sistemadebiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biblioteca/sistemadebiblioteca/SistemaDeBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.sistemadebiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
