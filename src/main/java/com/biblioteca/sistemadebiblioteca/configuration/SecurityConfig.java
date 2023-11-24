73d64bd9417877a896863d94a24accb4af7beafa
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-11-23 23:21:06 -0300

    configuration encoder password

--- src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
@@ -3,8 +3,14 @@ package com.biblioteca.sistemadebiblioteca.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +23,10 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 
 }
