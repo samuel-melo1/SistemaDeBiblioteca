966f3e8919216b3522443e6a23b78f985cf2e290
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-11-27 23:28:40 -0300

    configuration security

--- src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.biblioteca.sistemadebiblioteca.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,22 +15,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/api/create").permitAll()
-                        // Adicione outras configurações de autorização conforme necessário
+                        .requestMatchers(HttpMethod.POST,"/api/create").hasRole("USER")
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());
-
-        return http.build();
+                .build();
 
     }
-
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 
 }
