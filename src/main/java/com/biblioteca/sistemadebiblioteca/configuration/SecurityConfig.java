654acddd492d1cc4c863604bf1f4b3e778736567
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-11-28 22:26:54 -0300

    configure security

--- src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/configuration/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.biblioteca.sistemadebiblioteca.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,7 +22,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST,"/api/create").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
@@ -29,6 +32,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 
