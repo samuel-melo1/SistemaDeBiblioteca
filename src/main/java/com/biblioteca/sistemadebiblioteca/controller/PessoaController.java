77cde00473804201b26269f6f4776598f512cc8e
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-01 23:36:43 -0300

    method get all users implemented

--- src/main/java/com/biblioteca/sistemadebiblioteca/controller/PessoaController.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/controller/PessoaController.java
@@ -5,8 +5,11 @@ import com.biblioteca.sistemadebiblioteca.exceptions.EmailExistsException;
 import com.biblioteca.sistemadebiblioteca.model.Pessoa;
 import com.biblioteca.sistemadebiblioteca.service.PessoaService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +24,18 @@ public class PessoaController {
     public ResponseEntity create(@RequestBody @Valid PessoaDTO pessoaDTO) throws EmailExistsException {
          this.pessoaService.register(pessoaDTO);
          return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/listUsers")
+    public ResponseEntity<List<Pessoa>> list(){
+        try{
+            List<Pessoa> list = pessoaService.getAll();
+
+            if(list.isEmpty()){ return new ResponseEntity<>(HttpStatus.NO_CONTENT); }
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }catch (Exception exception){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
     }
 }
--- src/main/java/com/biblioteca/sistemadebiblioteca/service/PessoaService.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/service/PessoaService.java
@@ -1,24 +1,21 @@
 package com.biblioteca.sistemadebiblioteca.service;
 
-
 import com.biblioteca.sistemadebiblioteca.dto.PessoaDTO;
 import com.biblioteca.sistemadebiblioteca.exceptions.EmailExistsException;
 import com.biblioteca.sistemadebiblioteca.model.Pessoa;
 import com.biblioteca.sistemadebiblioteca.repository.PessoaRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
-
+import java.util.Optional;
 
 @Service
 public class PessoaService {
     private PessoaRepository pessoaRepository;
-    private PasswordEncoder passwordEncoder;
-    public PessoaService(PessoaRepository pessoaRepository, PasswordEncoder passwordEncoder){
+
+    public PessoaService(PessoaRepository pessoaRepository){
         this.pessoaRepository = pessoaRepository;
-        this.passwordEncoder = passwordEncoder;
+
     }
     public Pessoa register(PessoaDTO pessoaDTO) throws EmailExistsException {
         if(pessoaRepository.existsPessoaByEmail(pessoaDTO.email())){
