a663a792eff47a4ce25f59910e29d5661d73d5de
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-04 23:53:49 -0300

    delete book

--- src/main/java/com/biblioteca/sistemadebiblioteca/service/LivroService.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/service/LivroService.java
@@ -3,8 +3,12 @@ package com.biblioteca.sistemadebiblioteca.service;
 import com.biblioteca.sistemadebiblioteca.dto.LivroDTO;
 import com.biblioteca.sistemadebiblioteca.exceptions.LivroExistsException;
 import com.biblioteca.sistemadebiblioteca.model.Livro;
+import com.biblioteca.sistemadebiblioteca.model.Pessoa;
 import com.biblioteca.sistemadebiblioteca.repository.LivroRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LivroService {
@@ -21,6 +25,16 @@ public class LivroService {
         }
         Livro newBook = new Livro(livroDTO.titulo(),livroDTO.categoria());
         return repository.save(newBook);
+    }
+
+    @Transactional
+    public boolean deleteBook(int id_book){
+        Optional<Livro> livro_id = repository.findById(id_book);
+        if(livro_id.isEmpty()){
+            return false;
+        }
+        repository.deleteById(id_book);
+        return true;
     }
 
 }
