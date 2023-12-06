ae66e2ce88e52668cf6f2d5d2b169317057b7e57
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-05 22:43:04 -0300

    defined security to delete book

--- src/main/java/com/biblioteca/sistemadebiblioteca/security/configuration/SecurityConfig.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/security/configuration/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,"api/listUsers").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "api/deleteUsers/{id}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST,"api/book/createBook").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "api/boot/deleteBook/{id}").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
