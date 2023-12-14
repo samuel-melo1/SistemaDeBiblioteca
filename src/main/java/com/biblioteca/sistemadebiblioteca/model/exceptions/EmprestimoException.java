d1fe2d32bb5856c38353e441267309a5d98f2e40
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-13 23:30:30 -0300

    implemented exception emprestimo

---
+++ src/main/java/com/biblioteca/sistemadebiblioteca/model/exceptions/EmprestimoException.java
@@ -0,0 +1,8 @@
+package com.biblioteca.sistemadebiblioteca.model.exceptions;
+
+public class EmprestimoException extends  RuntimeException{
+
+    public EmprestimoException(String message){
+        super(message);
+    }
+}
