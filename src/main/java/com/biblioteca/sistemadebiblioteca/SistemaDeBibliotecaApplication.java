831dcbea7ab01b8b71dc1967aaf1b4157db9c58f
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-18 22:51:48 -0300

    swagger documentation implemented

--- src/main/java/com/biblioteca/sistemadebiblioteca/SistemaDeBibliotecaApplication.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/SistemaDeBibliotecaApplication.java
@@ -1,9 +1,12 @@
 package com.biblioteca.sistemadebiblioteca;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Library API", version = "1.0",description = "Library Management System"))
 public class SistemaDeBibliotecaApplication {
 
     public static void main(String[] args) {
