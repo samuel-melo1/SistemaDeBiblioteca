17576abda19ee4113dabd1fe3f4dd92c6bce5eea
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-10 23:29:43 -0300

    implemented categoria handler exception class

--- src/main/java/com/biblioteca/sistemadebiblioteca/config/handler/RestExceptionHandler.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/config/handler/RestExceptionHandler.java
@@ -1,8 +1,18 @@
 package com.biblioteca.sistemadebiblioteca.config.handler;
 
+import com.biblioteca.sistemadebiblioteca.config.exceptions.CategoriaException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CategoriaException.class)
+    private ResponseEntity<String> categoriaNotFoundHandler(CategoriaException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categoria not found!");
+    }
+
 }
