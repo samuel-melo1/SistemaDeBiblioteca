dbfa05b3115483c24e69ad589617b0a78a6072d3
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-12 23:50:56 -0300

    Implemented pessoa, categoria and livro exception handler

--- src/main/java/com/biblioteca/sistemadebiblioteca/config/infra/handler/LivroExceptionHandler.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/config/infra/handler/LivroExceptionHandler.java
@@ -1,27 +1,25 @@
 package com.biblioteca.sistemadebiblioteca.config.infra.handler;
 
-import com.biblioteca.sistemadebiblioteca.config.exceptions.categoriaException.CategoriaExistException;
-import com.biblioteca.sistemadebiblioteca.config.exceptions.categoriaException.CategoriaNotFoundException;
+import com.biblioteca.sistemadebiblioteca.config.exceptions.livroException.LivroExistsException;
+import com.biblioteca.sistemadebiblioteca.config.exceptions.livroException.LivroNotFoundException;
 import com.biblioteca.sistemadebiblioteca.config.infra.responsehandler.RestErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CategoriaExceptionHandler extends ResponseEntityExceptionHandler {
+public class LivroExceptionHandler {
 
-    @ExceptionHandler(CategoriaNotFoundException.class)
-    private ResponseEntity<RestErrorMessage> categoriaNotFoundHandler(CategoriaNotFoundException exception){
+    @ExceptionHandler(LivroNotFoundException.class)
+    public ResponseEntity<RestErrorMessage> livroNotFoundException(LivroNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
     }
 
-    @ExceptionHandler(CategoriaExistException.class)
-    private ResponseEntity<RestErrorMessage> categoriaExistHandler(CategoriaExistException exception){
+    @ExceptionHandler(LivroExistsException.class)
+    public ResponseEntity<RestErrorMessage> livroExistsException(LivroExistsException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.CONFLICT, exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(restErrorMessage);
     }
-
 }
--- src/main/java/com/biblioteca/sistemadebiblioteca/config/infra/handler/PessoaExceptionHandler.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/config/infra/handler/PessoaExceptionHandler.java
@@ -1,25 +1,24 @@
 package com.biblioteca.sistemadebiblioteca.config.infra.handler;
 
-import com.biblioteca.sistemadebiblioteca.config.exceptions.categoriaException.CategoriaExistException;
-import com.biblioteca.sistemadebiblioteca.config.exceptions.categoriaException.CategoriaNotFoundException;
+import com.biblioteca.sistemadebiblioteca.config.exceptions.pessoaException.PessoaEmailException;
+import com.biblioteca.sistemadebiblioteca.config.exceptions.pessoaException.PessoaNotFoundException;
 import com.biblioteca.sistemadebiblioteca.config.infra.responsehandler.RestErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CategoriaExceptionHandler extends ResponseEntityExceptionHandler {
+public class PessoaExceptionHandler {
 
-    @ExceptionHandler(CategoriaNotFoundException.class)
-    private ResponseEntity<RestErrorMessage> categoriaNotFoundHandler(CategoriaNotFoundException exception){
+    @ExceptionHandler(PessoaNotFoundException .class)
+    private ResponseEntity<RestErrorMessage> pessoaNotFoundHandler(PessoaNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
     }
 
-    @ExceptionHandler(CategoriaExistException.class)
-    private ResponseEntity<RestErrorMessage> categoriaExistHandler(CategoriaExistException exception){
+    @ExceptionHandler(PessoaEmailException.class)
+    private ResponseEntity<RestErrorMessage> pessoaExistsEmailHandler(PessoaEmailException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.CONFLICT, exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(restErrorMessage);
     }
