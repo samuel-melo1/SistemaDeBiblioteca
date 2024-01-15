a063909ef7978b4aaf1fa8f80f0d1e716ec98713
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-14 23:13:06 -0300

    created class tests livro service and emprestimo service

---
+++ src/test/java/com/biblioteca/sistemadebiblioteca/service/EmprestimoServiceTest.java
@@ -0,0 +1,12 @@
+package com.biblioteca.sistemadebiblioteca.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmprestimoServiceTest {
+
+    @Test
+    void emprestar() {
+    }
+}
---
+++ src/test/java/com/biblioteca/sistemadebiblioteca/service/LivroServiceTest.java
@@ -0,0 +1,20 @@
+package com.biblioteca.sistemadebiblioteca.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LivroServiceTest {
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+}
