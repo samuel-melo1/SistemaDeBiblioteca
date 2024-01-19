ab2e29d9cfe2e97509edfdc1bd13169a3423ab89
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-18 23:47:18 -0300

    implemented test to service CategoriaService

--- src/test/java/com/biblioteca/sistemadebiblioteca/service/CategoriaServiceTest.java
+++ src/test/java/com/biblioteca/sistemadebiblioteca/service/CategoriaServiceTest.java
@@ -1,13 +1,45 @@
 package com.biblioteca.sistemadebiblioteca.service;
 
+import com.biblioteca.sistemadebiblioteca.domain.dto.CategoriaDTO;
+import com.biblioteca.sistemadebiblioteca.domain.model.Categoria;
+import com.biblioteca.sistemadebiblioteca.domain.repository.CategoriaRepository;
+import com.biblioteca.sistemadebiblioteca.domain.service.CategoriaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class CategoriaServiceTest {
 
+    @Mock
+    private CategoriaRepository categoriaRepository;
+
+    @InjectMocks
+    CategoriaService categoriaService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    void create() {
+    @DisplayName("Should successfully register a new categoria")
+    void createCategoriaWhenExceptionFalse() {
+
+        CategoriaDTO categoriaDTO = new CategoriaDTO("Romance", "Narrativas centradas em relacionamentos amorosos");
+
+        Mockito.when(categoriaRepository.existsCategoriaByNome(categoriaDTO.nome())).thenReturn(false);
+        this.categoriaService.create(categoriaDTO);
+
+        verify(categoriaRepository, times(1)).save(any());
+
     }
 
     @Test
