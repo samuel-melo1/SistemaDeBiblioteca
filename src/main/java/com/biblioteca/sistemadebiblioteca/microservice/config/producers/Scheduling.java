8a6dc232b953dfa96b1f8c284f9086442e35603e
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-03-04 23:38:14 -0300

    creating class to schedule email sender

---
+++ src/main/java/com/biblioteca/sistemadebiblioteca/microservice/config/producers/Scheduling.java
@@ -0,0 +1,17 @@
+package com.biblioteca.sistemadebiblioteca.microservice.config.producers;
+
+import com.biblioteca.sistemadebiblioteca.domain.model.Emprestimo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class Scheduling {
+
+    public Scheduling(){
+        
+
+    }
+
+}
