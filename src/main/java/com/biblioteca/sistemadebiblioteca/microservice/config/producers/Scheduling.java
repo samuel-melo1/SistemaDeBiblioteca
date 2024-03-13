9b6ae3355366534cf8dfd33b50fcc4303e52e1ed
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-03-12 22:35:19 -0300

    implementation final method to send email when final date is coming.

--- src/main/java/com/biblioteca/sistemadebiblioteca/microservice/config/producers/Scheduling.java
+++ src/main/java/com/biblioteca/sistemadebiblioteca/microservice/config/producers/Scheduling.java
@@ -1,31 +1,57 @@
 package com.biblioteca.sistemadebiblioteca.microservice.config.producers;
 
+import com.biblioteca.sistemadebiblioteca.config.infra.exceptions.pessoaException.PessoaNotFoundException;
 import com.biblioteca.sistemadebiblioteca.domain.dto.EmailDTO;
 import com.biblioteca.sistemadebiblioteca.domain.model.Emprestimo;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import com.biblioteca.sistemadebiblioteca.repository.EmprestimoRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
-@EnableScheduling
+@Service
+@Slf4j
 public class Scheduling {
-    @Scheduled(cron = "0 0 6")
-    public void verifyDeadline(Emprestimo emprestimo){
-        if(emprestimo.getPrazo().equals(LocalDate.now())){
-            var emailDto = new EmailDTO();
-            emailDto.setUserId(emprestimo.getPessoa().getId_pessoa());
-            emailDto.setEmailTo(emprestimo.getPessoa().getEmail());
-            emailDto.setSubject("Lembrete: Devolução de Livro em Breve - " + emprestimo.getLivro().getTitulo());
-            emailDto.setText("Prezado(a) " + emprestimo.getPessoa().getNome() +
-                    ",\n\nEste é um lembrete amigável de que o livro que você pegou emprestado, intitulado \"" +
-                    emprestimo.getLivro().getTitulo() + "\", está prestes a vencer. Pedimos a gentileza de devolvê-lo até a data de vencimento para evitar possíveis penalidades.\n\n" +
-                    "Detalhes do Empréstimo:\n" +
-                    "- Título do Livro: " + emprestimo.getLivro().getTitulo() + "\n" +
-                    "- Data de Empréstimo: " + emprestimo.getData_emprestimo() + "\n" +
-                    "- Data de Vencimento: " + emprestimo.getPrazo() + "\n\n" +
-                    "Agradecemos pela sua atenção e cooperação.");
 
+
+    private EmprestimoRepository emprestimoRepository;
+    private RabbitTemplate rabbitTemplate;
+    public Scheduling(EmprestimoRepository emprestimoRepository, RabbitTemplate rabbitTemplate){
+        this.emprestimoRepository = emprestimoRepository;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    @Scheduled(cron = "0 0 6")
+    public void verifyDeadline(){
+        List<Emprestimo> listEmprestimo = emprestimoRepository.buscarEmprestimo();
+
+        for (Emprestimo emprestimo : listEmprestimo) {
+           if(emprestimo.getPrazo().minusDays(1).isEqual(LocalDate.now())){
+                var emailDto = new EmailDTO();
+                emailDto.setUserId(emprestimo.getPessoa().getId_pessoa());
+                emailDto.setEmailTo(emprestimo.getPessoa().getEmail());
+                emailDto.setSubject("Lembrete: Devolução de Livro em Breve - " + emprestimo.getLivro().getTitulo());
+                emailDto.setText("Prezado(a) " + emprestimo.getPessoa().getNome() +
+                        ",\n\nEste é um lembrete amigável de que o livro que você pegou emprestado, intitulado \"" +
+                        emprestimo.getLivro().getTitulo() + "\", está prestes a vencer. Pedimos a gentileza de devolvê-lo até a data de vencimento para evitar possíveis penalidades.\n\n" +
+                        "Detalhes do Empréstimo:\n" +
+                        "- Título do Livro: " + emprestimo.getLivro().getTitulo() + "\n" +
+                        "- Data de Empréstimo: " + emprestimo.getData_emprestimo() + "\n" +
+                        "- Data de Vencimento: " + emprestimo.getPrazo() + "\n\n" +
+                        "Agradecemos pela sua atenção e cooperação.");
+
+                rabbitTemplate.convertAndSend("", routingKey, emailDto);
+           }
         }
     }
 }
